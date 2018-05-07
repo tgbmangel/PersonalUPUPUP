66fced552b7d1badc6665c02133dea7e37fc0d44
Shr Zhu <tgbmangel@163.com>
2018-05-07 17:57:55 +0800

    Update mjtt.py

--- Meiju/mjtt.py
+++ Meiju/mjtt.py
@@ -19,11 +19,18 @@ HTML_content=p.text
 A=re.findall(ed2k_url,HTML_content)
 for x in A:
     print(x)
+'''
+os.chdir("D:\Program Files (x86)\Thunder Network\Thunder\Program\") 
+
+os.system("Thunder.exe -StartType:DesktopIcon "%s"" % a) 
+'''
 
 class Meiju():
     def __init__(self,home_url):
         self.home_url = home_url
         self.ed2k_url = re.compile('href="(ed2k.*?)"')
         self.coding_format='gbk'
-    def get_url(self):
-        r=requests.get(self.home_url)
+    def get_download_url(self):
+        rsp=requests.get(self.home_url)
+        rsp.encoding=self.coding_format
+        HTML_content = rsp.text
