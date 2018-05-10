72311fb19bbd28215f60ca72d0a7085cfd454465
Shr Zhu <tgbmangel@163.com>
2018-05-10 22:16:17 +0800

    Update mjtt.py

--- Meiju/mjtt.py
+++ Meiju/mjtt.py
@@ -1,35 +1,14 @@
 # -*- coding: utf-8 -*-
-# @Project : upupup 
+# @Project : upupup
 # @Time    : 2018/5/7 17:08
-# @Author  : 
+# @Author  :
 # @File    : mjtt.py
 # @Software: PyCharm Community Edition
 
 import requests
 import re
+import os
 
-#home_url="http://www.meijutt.com/content/meiju23109.html"
-home_url="http://www.meijutt.com/content/meiju22434.html"
-ed2k_url=re.compile('href="(ed2k.*?)"')
-p=requests.get(home_url)
-p.encoding='gbk'
-print(p.encoding)
-HTML_content=p.text
-
-h=BeautifulSoup(HTML_content,'lxml')
-ls=h.find_all('a',href=re.compile('ed2k'))
-for x in ls:
-    print(x)
-    print(x.attrs['href'])
-
-A=re.findall(ed2k_url,HTML_content)
-for x in A:
-    print(x)
-'''
-os.chdir("D:\Program Files (x86)\Thunder Network\Thunder\Program\") 
-
-os.system("Thunder.exe -StartType:DesktopIcon "%s"" % a) 
-'''
 
 class Meiju():
     def __init__(self,home_url):
@@ -37,6 +16,29 @@ class Meiju():
         self.ed2k_url = re.compile('href="(ed2k.*?)"')
         self.coding_format='gbk'
     def get_download_url(self):
+        # download_url_list=[]
         rsp=requests.get(self.home_url)
         rsp.encoding=self.coding_format
         HTML_content = rsp.text
+        __all_download_re = re.findall(self.ed2k_url, HTML_content)
+        return __all_download_re
+
+if __name__=="__main__":
+    home_url = "http://www.meijutt.com/content/meiju23109.html"
+    A=Meiju(home_url)
+    downloadurls=A.get_download_url()
+    downloaded_list=os.listdir("F:\MeiJu")
+    print("-"*50)
+    print('已下载清单：')
+    print(downloaded_list)
+    print("-" * 50)
+    os.chdir(r"D:\Program Files (x86)\Thunder Network\MiniThunder\Bin")
+    for x in downloadurls:
+        print("-" * 20)
+        print('获取到：{}'.format(x))
+        if x.split("|")[2] in downloaded_list:
+            print("检测到已下载，跳过！")
+        else:
+            print("启动下载器！")
+            os.system("start ThunderMini.exe -StartType:DesktopIcon \"%s\"" %x)
+    print("-" * 50)
