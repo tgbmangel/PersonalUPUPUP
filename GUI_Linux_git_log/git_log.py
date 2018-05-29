02ede384ff692ae3869abd083800f8f880291406
Shr Zhu <tgbmangel@163.com>
2018-05-29 14:26:25 +0800

    Update git_log.py

--- GUI_Linux_git_log/git_log.py
+++ GUI_Linux_git_log/git_log.py
@@ -14,14 +14,13 @@ class Chat_UI():
     def __init__(self):
         self.root=tk.Tk()
         self.root.title('Git Log')
-        self.root.geometry('800x600')
+        self.root.geometry('710x600')
         self.root.resizable(width=False, height=True)
         self.user_name= tk.StringVar()
         self.password = tk.StringVar()
         self.teaching_path = tk.StringVar()
         self.host_ip=tk.StringVar()
         self.git_path=tk.StringVar()
-
     def ui_grid(self,cntrl,row_column_tuple):
         cntrl.grid(row=row_column_tuple[0],column=row_column_tuple[1])
     def login_ssh(self):
@@ -42,6 +41,7 @@ class Chat_UI():
         try:
             self.ssh.connect(hostname=host_ip, port=22, username=host_user_name, password=host_password)
             self.login_button.config(state='disable',text='已登录')
+            self.host_ip_combobox.config(state='disable')
             self.login_out_button.config(state='active')
             if host_ip=="10.16.3.26":
                 self.git_path['values'] = (
@@ -65,6 +65,9 @@ class Chat_UI():
     def login_out(self):
         self.ssh.close()
         self.login_button.config(state='active', text='登录')
+        self.host_ip_combobox.config(state='active')
+        self.login_out_button.config(state='disable')
+
     def gitlog(self):
         if not self.git_path.get():
             messagebox.showinfo("path is null",'git path is null')
@@ -73,7 +76,7 @@ class Chat_UI():
         cmd1='git log --date=short  --pretty=format:"%h -%an- %ad -%s"'
         try:
             stdin, stdout, stderr = self.ssh.exec_command('{};{}'.format(cmd,cmd1)) #命令行需要在切换目录后马上执行不然会重新回到HOME目录，可能是session一样的原因
-            print(stderr.readlines())
+            # print(stderr.readlines())
             all_outs=stdout.readlines()
             self.git_log_text.delete(0.0,tk.END)
             for x in all_outs:
@@ -81,19 +84,19 @@ class Chat_UI():
         except Exception as e:
             messagebox.showinfo("info", e)
     def ui_controls(self):
-        logon_frame=tk.LabelFrame(self.root,width=300, height=100, text='登录')
-        host_ip_lable=tk.Label(logon_frame,text='ip addr:')
-        host_ip_combobox=ttk.Combobox(logon_frame,textvariable=self.host_ip)
-        host_ip_combobox["values"]=(
+        login_frame=tk.LabelFrame(self.root,width=300, height=100, text='登录',relief='groove')
+        host_ip_lable=tk.Label(login_frame,text='ip addr:')
+        self.host_ip_combobox=ttk.Combobox(login_frame,textvariable=self.host_ip)
+        self.host_ip_combobox["values"]=(
             '10.16.3.26',
             '10.16.3.10'
         )
-        user_name_lable=tk.Label(logon_frame,text='user:')
-        password_lable=tk.Label(logon_frame,text='password:')
-        user_name_entry=tk.Entry(logon_frame,textvariable=self.user_name)
-        password_entry=tk.Entry(logon_frame,textvariable=self.password,show='*')
-        self.login_button=tk.Button(logon_frame,text='登录',command=self.login_ssh)
-        self.login_out_button = tk.Button(logon_frame, text='退出', command=self.login_out,state='disable')
+        user_name_lable=tk.Label(login_frame,text='user:')
+        password_lable=tk.Label(login_frame,text='password:')
+        user_name_entry=tk.Entry(login_frame,textvariable=self.user_name,relief='groove')
+        password_entry=tk.Entry(login_frame,textvariable=self.password,show='*',relief='groove')
+        self.login_button=tk.Button(login_frame,text='登录',relief='flat',command=self.login_ssh)
+        self.login_out_button = tk.Button(login_frame, text='退出',relief='flat', command=self.login_out,state='disable')
         #
         path_git_frame=tk.LabelFrame(self.root,width=700, height=80,text='path')
         teaching_lable = tk.Label(path_git_frame, text='teaching path:')
@@ -104,16 +107,16 @@ class Chat_UI():
         ##
         text_frame=tk.LabelFrame(self.root,width=700,height=400,text='日志')
         self.git_log_text=scrolledtext.ScrolledText(text_frame,wrap=tk.WORD)
-        #
-        logon_frame.grid(row=0,column=0)
+        #布局
+        login_frame.grid(row=0,column=0)
         host_ip_lable.place(x=0,y=0)
-        host_ip_combobox.place(x=80,y=0)
+        self.host_ip_combobox.place(x=80,y=0)
         user_name_lable.place(x=0,y=20)
         user_name_entry.place(x=80,y=20)
         password_lable.place(x=0,y=40)
         password_entry.place(x=80,y=40)
-        self.login_button.place(x=240,y=20,height=22)
-        self.login_out_button.place(x=240,y=40,height=22)
+        self.login_button.place(x=250,y=20,height=22)
+        self.login_out_button.place(x=250,y=40,height=22)
         #
         path_git_frame.grid(row=1, column=0,columnspan=4)
         teaching_lable.place(x=0,y=0,width=100)
@@ -121,14 +124,23 @@ class Chat_UI():
         # teaching_entry.place(x=100,y=0,width=300)
         git_log_button.place(x=450,y=0,height=22)
         #
-        text_frame.grid(row=4,columnspan=4)
+        text_frame.grid(row=4,column=0,columnspan=4)
         self.git_log_text.place(x=0,width=695,height=370)
     def data_init(self):
         # self.teaching_path.set('/data/javaweb/smart-city-education-teaching')
         self.host_ip.set('10.16.3.26')
+    def close_window(self):
+        # messagebox.showinfo('info','window close')
+        try:
+            self.ssh.close()
+        except Exception as e:
+            pass
+        self.root.destroy()
     def run(self):
         self.ui_controls()
         self.data_init()
+        #监控窗口关闭事件
+        self.root.protocol('WM_DELETE_WINDOW',self.close_window)
         self.root.mainloop()
 if __name__ == '__main__':
     a=Chat_UI()
